7b4e9d3363362a8497e308dc7125f6d57faad92c
Khairul <e0309879@u.nus.edu>
2020-01-19 18:45:35 +0800

    Add CalculateCommand

--- src/main/java/seedu/address/logic/commands/calculate/CalculateCommand.java
+++ src/main/java/seedu/address/logic/commands/calculate/CalculateCommand.java
@@ -7,8 +7,12 @@ import seedu.address.logic.commands.commandresults.GlobalCommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.*;
+import seedu.address.model.team.TeamNumber;
 import seedu.address.model.time.EndTime;
+import seedu.address.model.time.StartTime;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -37,12 +41,51 @@ public class CalculateCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        PriorityQueue<PersonResult> resultQueue = new PriorityQueue<>();
+
+        PriorityQueue<PersonResult> resultQueue = new PriorityQueue<>((PersonResult person, PersonResult other) -> {
+            int categoryDifference = Category.getCategoryCode(person.getCategory())
+                    - Category.getCategoryCode(other.getCategory());
+            if (categoryDifference == 0) {
+                return person.getTimeTaken().compareTo(other.getTimeTaken());
+            } else {
+                return categoryDifference;
+            }
+        });
 
         ObservableList<PersonRegister> registers = model.getPersonRegisters();
         ObservableList<PersonStart> starts = model.getPersonStarts();
         ObservableList<EndTime> endTimes = model.getEndTimes();
         ObservableList<PersonEnd> ends = model.getPersonEnds();
+
+        for (PersonRegister personRegister: registers) {
+            Name name = personRegister.getName();
+            BibNumber bibNumber = personRegister.getBibNumber();
+            TeamNumber teamNumber = personRegister.getTeamNumber();
+            Category category = personRegister.getCategory();
+            StartTime startTime = null;
+            EndTime endTime = null;
+            Duration timeTaken;
+
+            for (PersonStart personStart : starts) {
+                if (personStart.getBibNumber().equals(bibNumber)) {
+                    startTime = personStart.getStartTime();
+                }
+            }
+            for (PersonEnd personEnd : ends) {
+                if (personEnd.getBibNumber().equals(bibNumber)) {
+                    endTime = endTimes.get(personEnd.getIndex().index);
+                }
+            }
+
+            if (startTime == null || endTime == null) {
+                startTime = new StartTime(Instant.MIN);
+                endTime = new EndTime(Instant.MAX);
+            }
+            timeTaken = Duration.between(startTime.getStartTime(), endTime.getEndTime());
+            PersonResult personResult = new PersonResult(name, bibNumber, teamNumber, category,
+                    startTime, endTime, timeTaken);
+            resultQueue.add(personResult);
+        }
 
         List<PersonResult> personResults = new ArrayList<>(resultQueue);
         model.setPersonResults(personResults);
