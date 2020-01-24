472da44bd85d30af256b2b1e1304ad1434e1530f
Sahilgat <nisgat@live.com>
2020-01-24 21:42:11 +0800

    Change category to include team, add team counter button

--- src/main/java/seedu/address/model/person/Category.java
+++ src/main/java/seedu/address/model/person/Category.java
@@ -9,20 +9,22 @@ public enum Category {
     NUS_MEN,
     NUS_WOMEN,
     OPEN_MEN,
-    OPEN_WOMEN;
+    OPEN_WOMEN,
+    TEAM;
 
     private int numberCode;
 
-    public static final String MESSAGE_CONSTRAINTS = "Category should only be the 4 category"
+    public static final String MESSAGE_CONSTRAINTS = "Category should only be the 5 category"
             + "codes: \n"
             + " 1: NUS Men \n"
-            + " 2: NUS Women \n 3: Open Men \n 4: Open Women";
+            + " 2: NUS Women \n 3: Open Men \n 4: Open Women \n 5: Team";
 
     static {
         NUS_MEN.numberCode = 1;
         NUS_WOMEN.numberCode = 2;
         OPEN_MEN.numberCode = 3;
         OPEN_WOMEN.numberCode = 4;
+        TEAM.numberCode = 5;
     }
 
     public static Category getCategoryFromString(String stringCategory) throws StringToCategoryConversionException {
@@ -35,6 +37,8 @@ public enum Category {
             return OPEN_MEN;
         case "OPEN_WOMEN":
             return OPEN_WOMEN;
+        case "TEAM":
+            return TEAM;
         default:
             throw new StringToCategoryConversionException();
         }
@@ -50,6 +54,8 @@ public enum Category {
                 return OPEN_MEN;
             case "4":
                 return OPEN_WOMEN;
+            case "5":
+                return TEAM;
             default:
                 throw new StringToCategoryConversionException();
         }
@@ -65,6 +71,8 @@ public enum Category {
                 return 3;
             case OPEN_WOMEN:
                 return 4;
+            case TEAM:
+                return 5;
             default:
                 return 0;
         }
